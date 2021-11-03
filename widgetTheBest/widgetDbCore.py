c2881114802d00e0150dc5781e8c7ae80deadacc
Kevin Sullivan <devjsullivan@gmail.com>
2021-11-02 20:05:25 -0400

    added create and list_add funcs

--- widgetTheBest/widgetDbCore.py
+++ widgetTheBest/widgetDbCore.py
@@ -1,11 +1,13 @@
 import sqlite3
+from datetime import datetime
+
 
 class widgetDbCore:
     def __init__(self):
         # Create a database in RAM and connect:
         self._mDb = sqlite3.connect(':memory:')
-        # get cursor object
-        cursor = self._mDb.cursor()
+        # get cursor object, create one for self so we don't have to keep getting it for other functions
+        self._mcursor = self._mDb.cursor()
         # create widgets table. This will be the body, or "Core" of our class
         # Properties have been defined as:
         # 1. Name(utf8 string, limited to 64 chars), data type: CHAR of size 64
@@ -18,6 +20,20 @@ class widgetDbCore:
                                Number INT, Created DATE unique, Updated DATE)
         ''')
         widgetDb.commit()
+    # add a row to the DB (dates will be updated automatically via datetime module
+    def create(self, nameIn: str, numIn: int):
+        current_date_and_time = datetime.now()
+        pretty_date_and_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        # (created and updated times are the same, so use pretty_date_and_time for both:)
+        self._mcursor.execute('''INSERT INTO widgets(Name, Number, Created, Updated)
+                          VALUES(?,?,?,?)''', (nameIn, numIn, pretty_date_and_time, pretty_date_and_time))
+    # output all widgets
+    def list_all(self):
+        widgets = self._mcursor.fetchall()
+        print("Here is a list of all widgets in your database (colums are Name, Number, Date/time created, Date/time updated:")
+        for w in widgets:
+            # w[0] returns the first column in the query (Name), row[1] returns Number column, etc:
+            print('{0} : {1}, {2}, {3}, {4}'.format(w[0], w[1], w[2], w[3], w[4]))
     def __del__(self):
         # when this class is deleted, close our DB. Because, hey, it's the right thing to do.
         self._mDb.close()
